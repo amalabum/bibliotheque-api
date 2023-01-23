4548ba252e97d87bd7319fd97e9e8162678c7421
amalabum <eliezermubalama@gmail.com>
2023-01-23 13:37:56 +0100

    api

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
     <?php 
-      header('Containt-type:Application/json');
+      header('Containt-Type:application/json');
       $livres=array("1"=>"livres","2"=>"livres","3"=>"livres","4"=>"livres");
       echo json_encode($livres);
     ?>
